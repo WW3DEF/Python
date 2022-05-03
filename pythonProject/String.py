703b41314b3cf2d1f026633b200514fe6dc30040
InDEF <zzangka1122@gmail.com>
2022-05-03 21:52:17 +0900

    문자열

---
+++ pythonProject/String.py
@@ -0,0 +1,17 @@
+aa = [10,20,30,40,50]
+print(aa[0])
+print(aa[1:3])
+print(aa[3:])
+print(aa[-1])
+
+ss = "파이썬최고"
+
+print(ss[0])
+print(ss[1:3])
+print(ss[3:])
+
+ss = "파이썬" + "최고"
+print(ss)
+ss = "파이썬최고" * 3
+print(ss)
+
